d94ff4ce6c29d9588e847b36f2f9714a9a6fe1cf
Danny Avery <danny.w.avery@gmail.com>
2017-11-09 20:35:43 -0700

    Add functionality for prev song

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,8 +10,19 @@
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if (nextSongIndex >= album.songs.length) { return; }
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+  });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const prevSongIndex = currentSongIndex - 1;
+    if (currentSongIndex === 0) { return; }
+
+    const prevSong = album.songs[prevSongIndex];
+    player.playPause(prevSong);
   });
 }
